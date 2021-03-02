ba91ccb30de256a84d5ef479760c291becb3a13c
Jeniffer Carvalho <jenicarvalho@live.com>
2021-03-02 19:47:48 -0300

    aula 37

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useForm } from "react-hook-form";
 function App() {
+  const { register, handleSubmit, watch, errors } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  }; 
+
+  const exterior = watch("exterior");
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Exterior?</label>
+      <input name="exterior" type="checkbox" ref={register} />
+      {exterior && (
+        <div>
+          <label>País</label>
+          <input type="text" name="pais" ref={register} />
+        </div>
+      )}
+      <input name="nome" ref={register({ required: true })} placeholder="nome"/>
+      {errors.nome && <p>Nome obrigatório</p>}
+
+      <label>Masc</label>
+      <input type="radio" ref={register} name="sexo" value="masculino" />
+     
+      <label>Fem</label>
+      <input type="radio" ref={register} name="sexo" value="feminino" />
+     
+      <label>aceito os termos</label>
+      <input type="checkbox" ref={register} name="aceito" />
+      
+      <button>enviar</button>
+    </form>
     </div>
   );
 }
